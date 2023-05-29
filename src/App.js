f41f6c5be6d3907a843228c3a65174d1283c34ab
tsopsericado <ricardotsopse@gmail.com>
2023-05-29 10:47:50 +0100

    the items component cart created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Hero />
+      <HeadlineCards/>
+      <Food/>
     </div>
-  )
+  );
 }
  export default App
 
