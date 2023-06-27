42816288669c665d6aa8520b23e24ca51c0a27f7
tsopsericado <ricardotsopse@gmail.com>
2023-06-27 20:30:41 +0100

    some style added

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -4,7 +4,7 @@ import { PaymentInputsWrapper, usePaymentInputs } from "react-payment-inputs";
 import { useNavigate, useParams } from "react-router-dom";
 import { useContext } from "react";
 import { MyContext } from "../context/Context";
-import './Payment.css'
+import "./Payment.css";
 
 function Payment() {
   const { formData } = useContext(MyContext);
@@ -33,8 +33,9 @@ function Payment() {
     wrapperProps,
   } = usePaymentInputs();
 
-  const handleSubmit = () => {
-    navigate("/");
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    navigate("/checkout");
   };
 
   return (
@@ -64,42 +65,46 @@ function Payment() {
           <form onSubmit={handleSubmit} className="form">
             <div className="formdiv">
               <h1 className="title">Make your payment</h1>
-             <div className="wrapper">
-              <PaymentInputsWrapper {...wrapperProps}>
-                <svg {...getCardImageProps({})} />
-                <div className="cardnum">
-                  <label htmlFor="cardnum">Card Number:</label>
-                  <Field name="cardNumber">
-                    {({ Field }) => <input {...getCardNumberProps({})} />}
-                  </Field>
-                </div>
-                <div className="expirydate">
-                  <label htmlFor="expirydate">Expiry Data:</label>
-                  <Field name="expiryDate">
-                    {({ field }) => <input {...getExpiryDateProps({})} />}
-                  </Field>
-                </div>
-                <div className="cvc">
-                  <label htmlFor="cvc">CVC:</label>
-                  <Field name="cvc">
-                    {({ field }) => <input {...getCVCProps({})} />}
-                  </Field>
-                </div>
-                <div className="formfooter">
-                <p>
-                  Package Name:
-                  <span className="text-yello-700">{purchaseFood.name}</span>
-                </p>
-                <p>
-                  Payment Amount:
-                  <span className="text-yello-500">${purchaseFood.price}</span>
-                </p>
-                </div>
-              </PaymentInputsWrapper>
-            </div>
+              <div className="wrapper">
+                <PaymentInputsWrapper {...wrapperProps}>
+                  <svg {...getCardImageProps({})} />
+                  <div className="cardnum">
+                    <label htmlFor="cardnum">Card Number:</label>
+                    <Field name="cardNumber">
+                      {({ Field }) => <input {...getCardNumberProps({})} />}
+                    </Field>
+                  </div>
+                  <div className="expirydate">
+                    <label htmlFor="expirydate">Expiry Data:</label>
+                    <Field name="expiryDate">
+                      {({ field }) => <input {...getExpiryDateProps({})} />}
+                    </Field>
+                  </div>
+                  <div className="cvc">
+                    <label htmlFor="cvc">CVC:</label>
+                    <Field name="cvc">
+                      {({ field }) => <input {...getCVCProps({})} />}
+                    </Field>
+                  </div>
+                  <div className="formfooter">
+                    <p>
+                      Package Name:
+                      <span className="text-yello-700">
+                        {purchaseFood.name}
+                      </span>
+                    </p>
+                    <p>
+                      Payment Amount:
+                      <span className="text-yello-500">
+                        ${purchaseFood.price}
+                      </span>
+                    </p>
+                  </div>
+                </PaymentInputsWrapper>
+              </div>
             </div>
             <button
-              type="button"
+              type="submit"
               onSubmit={handleSubmit}
               className="bg-orange-200 py-2"
             >
